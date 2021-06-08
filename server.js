19a1819d21b07be8510fb85f955a799dc281ee06
Manik Sharma <maniksharma98144@gmail.com>
2021-06-08 15:32:00 +0530

    minor changes

--- server.js
+++ server.js
@@ -17,6 +17,11 @@ const app = express();
 app.use(cors());
 app.use(compression());
 
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+
 app.use('/api', api);
 
 app.listen(PORT, () => {
