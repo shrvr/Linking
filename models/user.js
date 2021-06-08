a6205a3a7af316e55a762e1849fc3e78e89893fe
Jaydeep Patel <jaydeep7894@gmail.com>
2021-06-08 18:49:41 +0530

    added validation in user model

--- models/user.js
+++ models/user.js
@@ -1,20 +1,59 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    userId: String,
-    password: String,
-    age: Number,
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    userId: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        lowercase: true,
+        validate(val) {
+            if (!validator.isEmail(val)) throw new Error('UserID invalid.');
+        },
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    age: {
+        type: Number,
+        validate(val) {
+            if (val < 18)
+                throw new Error('Age must be greater than or equal to 18.');
+        },
+    },
     mobile: {
         type: String,
-        default: ""
+        trim: true,
+        default: '',
     },
     terms: {
         type: Boolean,
-        default: false
+        required: true,
+        default: false,
+    },
+});
+
+userSchema.pre('save', async function (next) {
+    const user = this;
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
     }
+    next();
 });
 
 mongoose.model('users', userSchema);
