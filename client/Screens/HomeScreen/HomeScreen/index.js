b231cd5723727367b764bee7d45aa055ed88e947
Shurvir Sisodiya <65063487+shrvr@users.noreply.github.com>
2021-06-08 16:24:55 +0530

    added homescreen

--- client/Screens/HomeScreen/HomeScreen/index.js
+++ client/Screens/HomeScreen/HomeScreen/index.js
@@ -1,11 +1,25 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Button, Text, View, TouchableOpacity } from "react-native";
+
+import Card from "./Card";
+import findTPImage from "../../../assets/option1.jpg";
+import findLPImage from "../../../assets/option2.jpg";
 
 const HomeScreen = () => {
-    return (
-        <View>
-            <Text>Home Screen</Text>
-        </View>
-        )
-}
+  return (
+    <View style={styles.container}>
+      <Card title="Find Travel Partner" image={findTPImage} />
+      <Card title="Find Near by People" image={findLPImage} />
+    </View>
+  );
+};
+
 export default HomeScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
