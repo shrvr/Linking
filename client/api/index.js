a5a4e35a84696ee0365ef3a2f2caa117fb6a3158
Manik Sharma <maniksharma98144@gmail.com>
2021-06-09 16:01:57 +0530

    api refinement in client

--- client/api/index.js
+++ client/api/index.js
@@ -1,3 +1,5 @@
+import { AsyncStorage } from '@react-native-community/async-storage';
+
 export class api {
     constructor() {
         this.baseUrl = "https://enigmatic-temple-22499.herokuapp.com/api";
@@ -5,6 +7,7 @@ export class api {
             "Content-Type": "application/json",
             Accept: "application/json",
         };
+        this.storageKey = "_user"
     }
 
     static signUp = (item) => {
@@ -31,5 +34,17 @@ export class api {
         ).then(res => {
             return res.json();
         });
+    }
+
+    static setStorageItem = (val) => {
+        AsyncStorage.setItem(this.storageKey, val);
+    }
+
+    static getStorageItem = () => {
+        AsyncStorage.getItem(this.storageKey)
+    }
+
+    static logout = () => {
+        AsyncStorage.clear();
     }
 }
