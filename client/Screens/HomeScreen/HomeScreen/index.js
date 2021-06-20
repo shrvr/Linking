c1615a93171223143be628331f7ae75517783cac
Jaydeep Patel <jaydeep7894@gmail.com>
2021-06-20 09:41:11 +0530

    added changes related to navigation

--- client/Screens/HomeScreen/HomeScreen/index.js
+++ client/Screens/HomeScreen/HomeScreen/index.js
@@ -1,44 +1,44 @@
-import React from "react";
-import { Text, View, TouchableOpacity, ImageBackground } from "react-native";
-import findTPImage from "../../../assets/option1.jpg";
-import CardStyle from "./CardStyle";
-import findLPImage from "../../../assets/option2.jpg";
+import React from 'react';
+import { Text, View, TouchableOpacity, ImageBackground } from 'react-native';
+import findTPImage from '../../../assets/option1.jpg';
+import CardStyle from './CardStyle';
+import findLPImage from '../../../assets/option2.jpg';
 
-const HomeScreen = ({ navigation }) => {
-  return (
-    <View style={CardStyle.container}>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.push("Places");
-        }}
-      >
-        <ImageBackground
-          style={CardStyle.image}
-          imageStyle={{
-            borderRadius: 30,
-          }}
-          source={findTPImage}
-        >
-          <Text style={CardStyle.textDesign}>Find Nearby Places</Text>
-        </ImageBackground>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => {
-          console.log("hello");
-        }}
-      >
-        <ImageBackground
-          style={CardStyle.image}
-          imageStyle={{
-            borderRadius: 30,
-          }}
-          source={findLPImage}
-        >
-          <Text style={CardStyle.textDesign}>Find Nearby Places</Text>
-        </ImageBackground>
-      </TouchableOpacity>
-    </View>
-  );
+const HomeScreen = ({ route, navigation }) => {
+    return (
+        <View style={CardStyle.container}>
+            <TouchableOpacity
+                onPress={() => {
+                    navigation.push('Places');
+                }}
+            >
+                <ImageBackground
+                    style={CardStyle.image}
+                    imageStyle={{
+                        borderRadius: 30,
+                    }}
+                    source={findTPImage}
+                >
+                    <Text style={CardStyle.textDesign}>Find Nearby Places</Text>
+                </ImageBackground>
+            </TouchableOpacity>
+            <TouchableOpacity
+                onPress={() => {
+                    console.log('hello');
+                }}
+            >
+                <ImageBackground
+                    style={CardStyle.image}
+                    imageStyle={{
+                        borderRadius: 30,
+                    }}
+                    source={findLPImage}
+                >
+                    <Text style={CardStyle.textDesign}>Find Nearby Places</Text>
+                </ImageBackground>
+            </TouchableOpacity>
+        </View>
+    );
 };
 
 export default HomeScreen;
--- client/Screens/HomeScreen/Places_Category/Places.js
+++ client/Screens/HomeScreen/Places_Category/Places.js
@@ -1,98 +1,106 @@
-import React from "react";
+import React from 'react';
 import {
-  View,
-  Text,
-  StyleSheet,
-  ScrollView,
-  TouchableOpacity,
-} from "react-native";
-import { MaterialIcons } from "@expo/vector-icons";
+    View,
+    Text,
+    StyleSheet,
+    ScrollView,
+    TouchableOpacity,
+} from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function Places({ navigation }) {
-  return (
-    <ScrollView>
-      <View style={styles.container}>
-        <TouchableOpacity
-          style={styles.block}
-          onPress={() => {
-            navigation.push("MapViewScreen", { text: "bar" });
-          }}
-        >
-          <MaterialIcons name="local-bar" size={100} color="#02a3bb" />
-          <Text style={styles.text}>Bar</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.block}
-          onPress={() => {
-            navigation.push("MapViewScreen", { text: "cafe" });
-          }}
-        >
-          <MaterialIcons name="local-cafe" size={100} color="#02a3bb" />
-          <Text style={styles.text}>Cafe</Text>
-        </TouchableOpacity>
+    return (
+        <ScrollView>
+            <View style={styles.container}>
+                <TouchableOpacity
+                    style={styles.block}
+                    onPress={() => {
+                        navigation.push('MapView', { text: 'bar' });
+                    }}
+                >
+                    <MaterialIcons
+                        name="local-bar"
+                        size={100}
+                        color="#02a3bb"
+                    />
+                    <Text style={styles.text}>Bar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.block}
+                    onPress={() => {
+                        navigation.push('MapView', { text: 'cafe' });
+                    }}
+                >
+                    <MaterialIcons
+                        name="local-cafe"
+                        size={100}
+                        color="#02a3bb"
+                    />
+                    <Text style={styles.text}>Cafe</Text>
+                </TouchableOpacity>
 
-        <TouchableOpacity
-          style={styles.block}
-          onPress={() => {
-            navigation.push("MapViewScreen", { text: "park" });
-          }}
-        >
-          <MaterialIcons name="park" size={100} color="#02a3bb" />
-          <Text style={styles.text}>Park</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.block}
-          onPress={() => {
-            navigation.push("MapViewScreen", { text: "cinema" });
-          }}
-        >
-          <MaterialIcons name="movie" size={100} color="#02a3bb" />
-          <Text style={styles.text}>Cinema</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.block}
-          onPress={() => {
-            navigation.push("MapViewScreen", { text: "hotel" });
-          }}
-        >
-          <MaterialIcons name="hotel" size={100} color="#02a3bb" />
-          <Text style={styles.text}>Hotel</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.block}
-          onPress={() => {
-            navigation.push("MapViewScreen", { text: "museum" });
-          }}
-        >
-          <MaterialIcons name="museum" size={100} color="#02a3bb" />
-          <Text style={styles.text}>Museum</Text>
-        </TouchableOpacity>
-      </View>
-    </ScrollView>
-  );
+                <TouchableOpacity
+                    style={styles.block}
+                    onPress={() => {
+                        navigation.push('MapView', { text: 'park' });
+                    }}
+                >
+                    <MaterialIcons name="park" size={100} color="#02a3bb" />
+                    <Text style={styles.text}>Park</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.block}
+                    onPress={() => {
+                        navigation.push('MapView', { text: 'cinema' });
+                    }}
+                >
+                    <MaterialIcons name="movie" size={100} color="#02a3bb" />
+                    <Text style={styles.text}>Cinema</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.block}
+                    onPress={() => {
+                        navigation.push('MapView', { text: 'hotel' });
+                    }}
+                >
+                    <MaterialIcons name="hotel" size={100} color="#02a3bb" />
+                    <Text style={styles.text}>Hotel</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.block}
+                    onPress={() => {
+                        navigation.push('MapView', { text: 'museum' });
+                    }}
+                >
+                    <MaterialIcons name="museum" size={100} color="#02a3bb" />
+                    <Text style={styles.text}>Museum</Text>
+                </TouchableOpacity>
+            </View>
+        </ScrollView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  block: {
-    margin: 40,
-    // padding: 10,
-    justifyContent: "center",
-    alignItems: "center",
-    // borderWidth: 2,
-    // borderColor: "black",
-  },
-  text: {
-    fontSize: 25,
-    padding: 5,
-    color: "black",
-    textAlign: "center",
-    justifyContent: "center",
-  },
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    block: {
+        margin: 40,
+        // padding: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        // borderWidth: 2,
+        // borderColor: "black",
+    },
+    text: {
+        fontSize: 25,
+        padding: 5,
+        color: 'black',
+        textAlign: 'center',
+        justifyContent: 'center',
+    },
 });
