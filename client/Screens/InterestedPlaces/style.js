e7c9f0618087279364dae966393dfee9ea21e038
ParthLPatel <parthlpatel2810@gmail.com>
2021-06-28 23:13:54 +0530

    modified style for action_icons

--- client/Screens/InterestedPlaces/style.js
+++ client/Screens/InterestedPlaces/style.js
@@ -3,22 +3,36 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   card: {
     flex: 1,
+    flexDirection: "row",
     width: "100%",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-between",
     marginVertical: 4,
     padding: 8,
+    // borderWidth: 2,
+    // borderColor: 'orange',
   },
   card_body: {
+    width: '60%',
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
+    textAlign: "justify",
+    // borderWidth: 2,
+    // borderColor: 'orange',
+  },
+  card_body_controls: { 
+    width: '40%',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    // borderWidth: 2,
+    // borderColor: 'green',
   },
   card_info: {
     flex: 7,
   },
   placeName: {
-    fontSize: 20,
+    fontSize: 16,
     textTransform: "capitalize",
     color: "black",
   },
