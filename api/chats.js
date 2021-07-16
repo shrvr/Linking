01d1c6ef539caa84c5a19636d7b1dd7d9e4651e0
Manik Sharma <maniksharma98144@gmail.com>
2021-07-17 01:44:13 +0530

    major changes

--- api/chats.js
+++ api/chats.js
@@ -5,45 +5,16 @@ const mongoose = require('mongoose');
 const auth = require('../middleware/auth');
 const Chat = mongoose.model('chats');
 
-router.get('/latest', auth ,async (req, res) => {
+router.get('/latest', auth, async (req, res) => {
     try {
         const chats = await Chat.find({
             conversationId: req.query.conversationId
-        }).sort({_id:-1}).limit(1);
+        }).sort({ _id: -1 }).limit(1);
         res.status(200).json(chats);
     } catch (err) {
         res.status(500).json(err);
     }
 });
-
-// router.post('/add', auth, async (req, res) => {
-//     const { targetUser, body } = req.body;
-//     const _user = req.user._id;
-//     const date = new Date();
-//     const chat = new Chat({
-//         _user,
-//         targetUser,
-//         body,
-//         date
-//     });
-//     try {
-//         await chat.save().then(response => {
-//             res.send("chat added");
-//         })
-//     } catch (err) {
-//         res.status(422).send("chat not added");
-//     }
-// })
-
-// router.get('/get', auth, async (req, res) => {
-//     const { targetUser } = req.query;
-
-//     try {
-//         await Chat.find({ _user: req.user._id, targetUser }).then(response => { res.send(response); });
-//     } catch (err) {
-//         res.status(422).send(err);
-//     }
-// });
 
 router.post('/', auth, async (req, res) => {
     const chat = new Chat(req.body);
--- api/places.js
+++ api/places.js
@@ -7,17 +7,42 @@ const Place = mongoose.model('places');
 const User = mongoose.model('users');
 
 router.post('/add', auth, async (req, res) => {
-    const { name, longitude, latitude } = req.body;
+    const { name, longitude, latitude, vicinity } = req.body;
     const _user = req.body._id
     const place = new Place({
         _user,
         name,
         longitude,
-        latitude
+        latitude,
+        vicinity
     });
 
     try {
         await place.save().then(response => { res.send("place added"); });
+    } catch (err) {
+        res.status(422).send(err);
+    }
+});
+
+router.post('/toggleShare', auth, async (req, res) => {
+    const { _trip, share } = req.body;
+    try {
+        await Place.findByIdAndUpdate(_trip, { share })
+            .then(response => {
+                res.send("updated")
+            })
+    } catch (err) {
+        res.status(422).send(err);
+    }
+});
+
+router.post('/delete', auth, async (req, res) => {
+    const { _trip } = req.body;
+    try {
+        await Place.findByIdAndDelete(_trip)
+            .then(response => {
+                res.send("Trip deleted successfully")
+            })
     } catch (err) {
         res.status(422).send(err);
     }
@@ -37,50 +62,29 @@ router.get('/all', auth, async (req, res) => {
 router.get('/usersbyTripId', auth, async (req, res) => {
     const { _trip } = req.query;
     try {
+        const blocks = await Block.find({ from: req.user._id })
         await Place.findById(_trip)
             .then(async response => {
-                await Place.find({ name: response.name, longitude: response.longitude, latitude: response.latitude })
+                await Place.find({ name: response.name, longitude: response.longitude, latitude: response.latitude, share: true })
                     .then(async resp => {
                         let users = [];
-                        resp.map(async val => {
-                            await User.findOne({ _id: val._user }).then(async details => {
-                                const block = await Block.find({ from: req.user._id })
-                                if (block)
-                                    if (res.statusCheck) {
-                                        if (details._id != res.to) {
-                                            users.push(details);
-                                        }
-                                    } else {
-                                        users.push(details);
-                                    }
+                        for (let i = 0; i < resp.length; i++) {
+                            await User.findOne({ _id: resp[i]._user }).then(details => {
+                                if (details._id == req.user._id)
+                                    continue
+
+                                let block = blocks.find(ele => ele.to = details._id)
+                                if (block && block.statusCheck) {
+                                    users.push(details);
+                                }
                                 else
                                     users.push(details);
                             })
-                        })
-                        res.send(users)
+                        }
+                        res.send(users);
                     })
             })
     } catch (err) {
         res.status(422).send(err);
     }
 });
-/*
-const { _trip } = req.query;
-    try {
-        await Place.findById(_trip)
-            .then(async response => {
-                await Place.find({ name: response.name, longitude: response.longitude, latitude: response.latitude })
-                    .then(async resp => {
-                        let users = [];
-                        resp.map(async val => {
-                            await User.findOne({ _id: val._user }).then(details => {
-                                users.push(details);
-                            })
-                        })
-                        res.send(users)
-                    })
-            })
-    } catch (err) {
-        res.status(422).send(err);
-    }
-    */
