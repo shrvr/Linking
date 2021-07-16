6bd51be988de486135d52b8b8a24443c65d90706
Manik Sharma <maniksharma98144@gmail.com>
2021-07-17 02:01:10 +0530

    block updated

--- api/block.js
+++ api/block.js
@@ -7,15 +7,24 @@ const mongoose = require('mongoose');
 const Block = mongoose.model('block');
 
 router.post('/block', auth, async (req, res) => {
-    const { recieverId, statusCheck } = req.body;
-    const block = new Block({
-        from: req.body._id,
-        to: recieverId,
-        statusCheck,
-    });
+    const { receiverId, statusCheck } = req.body;
     try {
-        await block.save().then(response => { res.send("User Blocked"); });
-    } catch (err) {
+        const block = await Block.findOne({ from: req.body._id, to: receiverId, });
+        if (block)
+            await Block.findByIdAndUpdate(block._id, { statusCheck })
+                .then(response => {
+                    res.status(200).json(response);
+                });
+        else {
+            const block = new Block({
+                from: req.body._id,
+                to: receiverId,
+                statusCheck,
+            });
+            await block.save().then(response => { res.status(200).json(response); });
+        }
+    }
+    catch (err) {
         res.status(422).send(err);
     }
 });
