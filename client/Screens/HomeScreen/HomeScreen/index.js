c944014b28d0b0e47a7ee3794b25a8aad01c8dda
GurjitVohra <gurjitvohra@gmail.com>
2021-07-19 22:01:21 +0530

    Minor Changes

--- client/Screens/HomeScreen/HomeScreen/index.js
+++ client/Screens/HomeScreen/HomeScreen/index.js
@@ -1,44 +1,44 @@
-import React from 'react';
-import { Text, View, TouchableOpacity, ImageBackground } from 'react-native';
-import findTPImage from '../../../assets/option1.jpg';
-import CardStyle from './CardStyle';
-import findLPImage from '../../../assets/option2.jpg';
+import React from "react";
+import { Text, View, TouchableOpacity, ImageBackground } from "react-native";
+import findTPImage from "../../../assets/option1.jpg";
+import CardStyle from "./CardStyle";
+import findLPImage from "../../../assets/option2.jpg";
 
 const HomeScreen = ({ route, navigation }) => {
-    return (
-        <View style={CardStyle.container}>
-            <TouchableOpacity
-                onPress={() => {
-                    navigation.push('Places');
-                }}
-            >
-                <ImageBackground
-                    style={CardStyle.image}
-                    imageStyle={{
-                        borderRadius: 30,
-                    }}
-                    source={findTPImage}
-                >
-                    <Text style={CardStyle.textDesign}>Find Nearby Places</Text>
-                </ImageBackground>
-            </TouchableOpacity>
-            <TouchableOpacity
-                onPress={() => {
-                    console.log('hello');
-                }}
-            >
-                <ImageBackground
-                    style={CardStyle.image}
-                    imageStyle={{
-                        borderRadius: 30,
-                    }}
-                    source={findLPImage}
-                >
-                    <Text style={CardStyle.textDesign}>Find Nearby Places</Text>
-                </ImageBackground>
-            </TouchableOpacity>
-        </View>
-    );
+  return (
+    <View style={CardStyle.container}>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.push("Places");
+        }}
+      >
+        <ImageBackground
+          style={CardStyle.image}
+          imageStyle={{
+            borderRadius: 30,
+          }}
+          source={findTPImage}
+        >
+          <Text style={CardStyle.textDesign}>Find Nearby Places</Text>
+        </ImageBackground>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          console.log("hello");
+        }}
+      >
+        <ImageBackground
+          style={CardStyle.image}
+          imageStyle={{
+            borderRadius: 30,
+          }}
+          source={findLPImage}
+        >
+          <Text style={CardStyle.textDesign}>Find Nearby People</Text>
+        </ImageBackground>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 export default HomeScreen;
